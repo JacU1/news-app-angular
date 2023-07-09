1f80fe1d5fe8bc9b368286484efb1adcd980b5f0
Jacek <jacunio122@gmail.com>
2023-07-09 22:14:15 +0200

    Login + singup changes + Base Abstract

--- src/app/views/sign-up/sign-up.module.ts
+++ src/app/views/sign-up/sign-up.module.ts
@@ -1,11 +1,11 @@
 import { CoreModule } from '../../core/core.module';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
 import {CommonModule} from "@angular/common";
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from 'src/app/shared/services/auth/auth-service';
 import { SignUpComponent } from './sign-up.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -13,8 +13,8 @@ import { SignUpComponent } from './sign-up.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
     ReactiveFormsModule,
-    CarouselModule,
     SharedModule,
     CoreModule,
   ],
