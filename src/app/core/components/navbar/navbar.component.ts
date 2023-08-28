cf8d147562018088af1ec75023c73b96e5c3514a
Jacek <jacunio122@gmail.com>
2023-08-28 14:46:46 +0200

    Small styling changes

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from 'src/app/shared/services/auth/auth-service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
